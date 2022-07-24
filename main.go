d6bae9cd5b76f8619a0a1fb66564c145a5b8210f
らいちんゆ <raitosongwei@gmail.com>
2022-07-24 11:35:27 +0900

    Add files via upload

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"os"
-	"github.com/raitochinyu/LINE-test/handler"
+	// "github.com/raitochinyu/LINE-test/handler"
+	"./handler"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("てすと")
-	http.HandleFunc("/", handler.HelloHandler)
-	http.HandleFunc("/callback", handler.LINEHandler)
+	fmt.Println("起動します。")
+	go http.HandleFunc("/", handler.resultHandler)
+	go http.HandleFunc("/callback", handler.LINEHandler)
 
 	port := os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("ポートが取得できませんでした。")
+		port = "5000"
+	}
+	fmt.Println(port)
 	addr := ":" + port
 
 	http.ListenAndServe(addr, nil)
